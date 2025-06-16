df1a6e60d06d3c9ebf705fa48bf2c6ab9ab1d6e8
Vladislav Shemet <specnaz790@mail.ru>
2025-06-17 00:39:45 +0300

    Меню

--- js/accardion.js
+++ js/accardion.js
@@ -3,6 +3,7 @@ const headers = document.querySelectorAll('.price__header');
 headers.forEach(header => {
   header.addEventListener('click', () => {
     const content = header.nextElementSibling;
+    const svg = header.querySelector('.accordion__svg'); 
     const isOpen = content.style.maxHeight;
 
     if (isOpen) {
@@ -10,11 +11,18 @@ headers.forEach(header => {
       content.style.maxHeight = null;
       content.classList.remove('open');
       header.classList.remove('transparent');
+      svg?.classList.remove('rotated');
     } else {
       // Открываем
       content.style.maxHeight = content.scrollHeight + 'px';
       content.classList.add('open');
       header.classList.add('transparent');
+      svg?.classList.add('rotated');
     }
   });
 });
+
+
+
+ 
+
---
+++ js/main.js
@@ -0,0 +1,33 @@
+/* Burger */
+
+  const burger = document.getElementById('burger');
+  const mobileNav = document.getElementById('mobileNav');
+  const overlay = document.getElementById('overlay');
+  const header = document.querySelector('.header');
+  const body = document.body;
+
+  function closeMenu() {
+    burger.classList.remove('active');
+    mobileNav.classList.remove('open');
+    overlay.classList.remove('show');
+    header.classList.remove('menu-open');
+    body.classList.remove('menu-open');
+  }
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    mobileNav.classList.toggle('open');
+    overlay.classList.toggle('show');
+    header.classList.toggle('menu-open');
+    body.classList.toggle('menu-open');
+  });
+
+  overlay.addEventListener('click', closeMenu);
+
+ 
+  mobileNav.addEventListener('click', (e) => {
+    if (e.target.tagName === 'A') {
+      closeMenu();
+    }
+  });
+
