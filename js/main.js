4414058cebdc4804d149cc174ea98a93658640b7
Vladislav <vlad.osmanov97@mail.ru>
2025-06-21 20:02:54 +0300

    add

--- js/main.js
+++ js/main.js
@@ -1,47 +1,18 @@
-/* Burger */
-
-<<<<<<< HEAD
-const burger = document.getElementById('burger');
-const mobileNav = document.getElementById('mobileNav');
-const overlay = document.getElementById('overlay');
-const header = document.querySelector('.header');
-const body = document.body;
-=======
-
-
+// Бургер
+document.addEventListener('DOMContentLoaded', () => {
   const burger = document.getElementById('burger');
   const mobileNav = document.getElementById('mobileNav');
   const overlay = document.getElementById('overlay');
   const header = document.querySelector('.header');
   const body = document.body;
->>>>>>> ccfbdf4ab9f7875d6fd60b14dd9aebec8e44b9e0
 
-function closeMenu() {
-  burger.classList.remove('active');
-  mobileNav.classList.remove('open');
-  overlay.classList.remove('show');
-  header.classList.remove('menu-open');
-  body.classList.remove('menu-open');
-}
-
-burger.addEventListener('click', () => {
-  burger.classList.toggle('active');
-  mobileNav.classList.toggle('open');
-  overlay.classList.toggle('show');
-  header.classList.toggle('menu-open');
-  body.classList.toggle('menu-open');
-});
-
-overlay.addEventListener('click', closeMenu);
-
-
-mobileNav.addEventListener('click', (e) => {
-  if (e.target.tagName === 'A') {
-    closeMenu();
+  function closeMenu() {
+    burger.classList.remove('active');
+    mobileNav.classList.remove('open');
+    overlay.classList.remove('show');
+    header.classList.remove('menu-open');
+    body.classList.remove('menu-open');
   }
-<<<<<<< HEAD
-});
-=======
 
   burger.addEventListener('click', () => {
     burger.classList.toggle('active');
@@ -53,17 +24,14 @@ mobileNav.addEventListener('click', (e) => {
 
   overlay.addEventListener('click', closeMenu);
 
- 
   mobileNav.addEventListener('click', (e) => {
     if (e.target.tagName === 'A') {
       closeMenu();
     }
   });
-  
->>>>>>> ccfbdf4ab9f7875d6fd60b14dd9aebec8e44b9e0
+});
 
 /* map Yandex   */
-
 {
 
   const map = document.getElementById('yandex-map');
