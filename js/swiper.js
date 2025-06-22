42b2fc1ca7375daafea4eaf48eaf57a0715b7872
Vladislav <vlad.osmanov97@mail.ru>
2025-06-22 10:09:50 +0300

    add

--- js/swiper.js
+++ js/swiper.js
@@ -3,13 +3,17 @@ const swiper = new Swiper('.swiper', {
 
   loop: true,
 
+  slidesPerView: 1,     // показывать по 1 слайду
+  slidesPerGroup: 1,    // прокручивать по 1 слайду
+
   autoplay: {
-    delay: 5000, 
-    disableOnInteraction: false 
+    delay: 5000,
+    disableOnInteraction: false
   },
 
   pagination: {
     el: '.swiper-pagination',
     clickable: true
   },
+  loopAdditionalSlides: 0,
 });
