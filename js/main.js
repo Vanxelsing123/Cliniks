e07d51b2d923b1873f36295fd9e303bf8006b5fb
Vladislav <vlad.osmanov97@mail.ru>
2025-06-22 09:49:20 +0300

    add

--- js/main.js
+++ js/main.js
@@ -1,46 +1,17 @@
 // Бургер
-document.addEventListener('DOMContentLoaded', () => {
-  const burger = document.getElementById('burger');
-  const mobileNav = document.getElementById('mobileNav');
-  const overlay = document.getElementById('overlay');
-  const header = document.querySelector('.header');
-  const body = document.body;
-
-  function closeMenu() {
-    burger.classList.remove('active');
-    mobileNav.classList.remove('open');
-    overlay.classList.remove('show');
-    header.classList.remove('menu-open');
-    body.classList.remove('menu-open');
-  }
-
-  burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
-    mobileNav.classList.toggle('open');
-    overlay.classList.toggle('show');
-    header.classList.toggle('menu-open');
-    body.classList.toggle('menu-open');
-  });
-
-  overlay.addEventListener('click', closeMenu);
-
-  mobileNav.addEventListener('click', (e) => {
-    if (e.target.tagName === 'A') {
-      closeMenu();
-    }
-  });
-});
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("burger").addEventListener("click", function () {
+    document.querySelector("header").classList.toggle("open")
+  })
+})
 
 /* map Yandex   */
-{
-
-  const map = document.getElementById('yandex-map');
-  map.parentElement.addEventListener('click', () => {
-    map.style.pointerEvents = 'auto';
-  });
-  map.parentElement.addEventListener('mouseleave', () => {
-    map.style.pointerEvents = 'none';
-  });
-}
+const map = document.getElementById('yandex-map');
+map.parentElement.addEventListener('click', () => {
+  map.style.pointerEvents = 'auto';
+});
+map.parentElement.addEventListener('mouseleave', () => {
+  map.style.pointerEvents = 'none';
+});
 
 
