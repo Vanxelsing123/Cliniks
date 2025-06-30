005b058fc09f759965eea205c466cc19e9b99042
Vladislav Shemet <specnaz790@mail.ru>
2025-06-30 19:39:34 +0300

    adaptiv canculator

---
+++ js/calculator-swiper.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.calculator-swiper', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+   
+  });
