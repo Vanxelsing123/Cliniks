dcad2a9c8edbb6787142665469005d55c605eb90
Vladislav <vlad.osmanov97@mail.ru>
2025-07-01 19:00:04 +0300

    add

---
+++ js/vacancies.js
@@ -0,0 +1,25 @@
+const openBtn = document.getElementById('openModalBtn');
+const modal = document.getElementById('modal');
+const closeBtn = document.getElementById('closeModalBtn');
+
+openBtn.addEventListener('click', () => {
+  modal.style.display = 'flex';
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+// Закрытие по клику вне модального окна
+modal.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
+
+// Закрытие по Esc
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && modal.style.display === 'flex') {
+    modal.style.display = 'none';
+  }
+});
